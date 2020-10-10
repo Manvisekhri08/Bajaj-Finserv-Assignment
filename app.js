a1d014754d3af0607bda47a6cdb94a01b2eb581e
Manvisekhri08 <manvisekhri08@gmail.com>
2020-10-11 00:43:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require('dotenv/config')
+const hbs = require("hbs")
 
 const app = express();
 const Patients = require("./models/patient")
 
+const publicDirectory = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./templates/views");
+// Setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set("views", viewsPath);
 
+// Setup static directory to serve
+app.use(express.static(publicDirectory));
 
-const connectionURL = process.env.DB_CONNECTION
+const connectionURL = "mongodb+srv://Manvi:sSwe8Gg5kZaLeyQW@cluster0.4r8kw.mongodb.net/Patients?retryWrites=true&w=majority"
 
 //Connecting to DataBase
 mongoose.connect(connectionURL,{ useUnifiedTopology: true, useNewUrlParser: true})
@@ -28,15 +35,31 @@ const server = app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
 });
 
-app.get('/', (req, res,next) => {
-    Patients.find({Mobno: '90875564332'}).then(data => {
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Patient Records',
+    })
+})
+
+
+//Fetching Patient Details
+app.get('/data', (req, res) => {
+    const MobNo = req.query.Mobno
+    if (!req.query.Mobno) {
+    return res.send({
+      error: 'You must provide a Mobile Number'
+    })
+  }
+    Patients.find({Mobno: MobNo}).then(data => {
         if (data) {
             res.status(200).json(data);
         } else {
-            console.log('Data not found');
+            res.status(404).json({ message: 'Data doesnot exist! Please check your Mobile Number Again' });
         }
     }).catch(error => {
-        console.log('Data not found');
+         res.status(500).json({
+        message:  'Data doesnot exist! Please check your Mobile Number Again'
+      });
       });
     }) 
 
