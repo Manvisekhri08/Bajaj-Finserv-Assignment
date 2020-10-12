d630f58eb847b4072e22736adca0bde4dd8c5cf0
Manvisekhri08 <manvisekhri08@gmail.com>
2020-10-12 14:21:17 +0530

    update

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-// const path = require("path");
+ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const hbs = require("hbs")
+ const hbs = require("hbs")
 
 require('dotenv/config')
 
 const app = express();
 const Patients = require("./models/patient")
 
-// const publicDirectory = path.join(__dirname, "./public");
-// const viewsPath = path.join(__dirname, "./templates/views");
+const publicDirectory = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./templates/views");
 // Setup handlebars engine and view location
-// app.set('view engine', 'hbs')
-// app.set("views", viewsPath);
+app.set('view engine', 'hbs')
+app.set("views", viewsPath);
 
-// // Setup static directory to serve
-// app.use(express.static(publicDirectory));
+// Setup static directory to serve
+app.use(express.static(publicDirectory));
 
 const connectionURL = process.env.DB_CONNECTION
 
